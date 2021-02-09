a36673a165af3c0574392edee4fd72d57e23be95
Guilherme <guilherme_devz@hotmail.com>
2021-02-09 20:45:14 -0300

    ADD: creating a instance for the routes of users

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,7 @@ const multer = require("../app/middlewares/multer");
 
 const sessionController = require("../app/controllers/sessionController");
 const userController = require("../app/controllers/userController");
-
+/* 
 // login/logout
 routes.get("/login", sessionController.loginForm);
 routes.post("/login", sessionController.login);
@@ -15,13 +15,15 @@ routes.get("/forgot-password", sessionController.forgotForm);
 routes.get("/password-reset", sessionController.resetForm);
 routes.post("/forgot-password", sessionController.forgot);
 routes.post("/password-reset", sessionController.reset);
+*/
 
 // user register userController
 routes.get("/register", userController.registerForm);
+/*
 routes.post("/register", userController.post);
 
 routes.get("/", userController.show);
 routes.update("/", userController.update);
-routes.delete("/", userController.delete);
+routes.delete("/", userController.delete); */
 
 module.exports = routes;
