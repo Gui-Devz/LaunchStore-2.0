f0049efd78c844b31896b99ea2be35e45eb7d6a6
Guilherme <guilherme_devz@hotmail.com>
2021-02-10 17:57:05 -0300

    ADD: creating the 'post' function

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,8 +1,18 @@
-const { formatPricing, formatPath } = require("../../lib/utils");
-const Product = require("../models/Product");
+const {
+  formatPricing,
+  formatPath,
+  validationOfBlankForms,
+} = require("../../lib/utils");
+const User = require("../models/User");
 
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register");
+  },
+
+  async post(req, res) {
+    const userID = await User.create(req.body);
+
+    return res.redirect(`users/${userID}`);
   },
 };
