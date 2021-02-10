8c4a530aa361a691cea70e6ea0416a19fa86f293
Guilherme <guilherme_devz@hotmail.com>
2021-02-10 20:37:51 -0300

    add: creating the function 'show' and using the req.session when 'post'

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,18 +1,33 @@
-const {
-  formatPricing,
-  formatPath,
-  validationOfBlankForms,
-} = require("../../lib/utils");
 const User = require("../models/User");
+const { formatCpfCnpj, formatCep } = require("../../lib/utils");
 
 module.exports = {
   registerForm(req, res) {
     return res.render("user/register");
   },
 
+  async show(req, res) {
+    const { userId: id } = req.session;
+
+    const results = await User.findOne({ where: { id } });
+    const user = results.rows[0];
+
+    if (!user)
+      return res.render("user/register", {
+        error: "Usuário não encontrado!",
+      });
+
+    user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj);
+    user.cep = formatCep(user.cep);
+
+    return res.render("user/index", { user });
+  },
+
   async post(req, res) {
     const userID = await User.create(req.body);
 
-    return res.redirect(`users/${userID}`);
+    req.session.userId = userID;
+
+    return res.redirect(`/${userID}`);
   },
 };
