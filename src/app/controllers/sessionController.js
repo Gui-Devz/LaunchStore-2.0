c66bfe3994a73a9cb9c2bcbe56966d506a5297f9
Guilherme <guilherme_devz@hotmail.com>
2021-02-12 18:31:06 -0300

    ADD: creating the functions 'forgot' and 'forgotForm'

--- src/app/controllers/sessionController.js
+++ src/app/controllers/sessionController.js
@@ -1,9 +1,16 @@
+const crypto = require("crypto");
+const User = require("../models/User");
+
 module.exports = {
   loginForm(req, res) {
     const { errorMessage } = req;
 
     console.log(req.errorCreate);
     return res.render("session/login");
+  },
+
+  forgotForm(req, res) {
+    return res.render("session/forgot-password");
   },
 
   async login(req, res) {
@@ -20,5 +27,25 @@ module.exports = {
     } catch (error) {
       console.error(error);
     }
+  },
+
+  forgot(req, res) {
+    const user = req.user;
+    // um token para esse usuário
+    const token = crypto.randomBytes(20).toString('hex');
+  
+    //criar uma expiração do token
+    let now = new Date();
+    now = now.setHours(now.getHours()+1);
+
+    await User.update(user.id,{
+      reset_token: token,
+      reset_token_expires: now
+    })
+
+    //enviar um email com um link de recuperação de senha.
+
+    //avisar o usuário que enviamos o email.
+
   },
 };
