97aa2648e99c18d50e9f1f9d7f3eae373f24d9c3
Guilherme <guilherme_devz@hotmail.com>
2021-02-12 17:11:18 -0300

    ADD: creating the routes for login and logout and implementing their validators and middlewares

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,26 +5,27 @@ const multer = require("../app/middlewares/multer");
 const sessionController = require("../app/controllers/sessionController");
 const userController = require("../app/controllers/userController");
 
-const Validator = require("../app/validators/user");
-/* 
+const UserValidator = require("../app/validators/user");
+const SessionValidator = require("../app/validators/session");
+const { isLoggedRedirectToUsers } = require("../app/middlewares/session");
+
 // login/logout
-routes.get("/login", sessionController.loginForm);
-routes.post("/login", sessionController.login);
+routes.get("/login", isLoggedRedirectToUsers, sessionController.loginForm);
+routes.post("/login", SessionValidator.login, sessionController.login);
 routes.post("/logout", sessionController.logout);
 
-// reset password / forgot
-routes.get("/forgot-password", sessionController.forgotForm);
-routes.get("/password-reset", sessionController.resetForm);
-routes.post("/forgot-password", sessionController.forgot);
-routes.post("/password-reset", sessionController.reset);
-*/
+// // reset password / forgot
+// routes.get("/forgot-password", sessionController.forgotForm);
+// routes.get("/password-reset", sessionController.resetForm);
+// routes.post("/forgot-password", sessionController.forgot);
+// routes.post("/password-reset", sessionController.reset);
 
 // user register userController
 routes.get("/register", userController.registerForm);
-routes.post("/register", Validator.post, userController.post);
+routes.post("/register", UserValidator.post, userController.post);
 
-routes.get("/", Validator.show, userController.show);
-routes.put("/", Validator.update, userController.update);
+routes.get("/", UserValidator.show, userController.show);
+routes.put("/", UserValidator.update, userController.update);
 /* routes.delete("/", userController.delete);
  */
 module.exports = routes;
