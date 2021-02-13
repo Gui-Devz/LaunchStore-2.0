9037fd52b3ee97e92e587129ef1b81c8d24c65cd
Guilherme <guilherme_devz@hotmail.com>
2021-02-13 18:20:54 -0300

    ADD: adding the middlewares validators fo the 'post', 'delete' and 'edit' routes

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,16 +5,26 @@ const multer = require("../app/middlewares/multer");
 const productsController = require("../app/controllers/productsController");
 const searchController = require("../app/controllers/searchController");
 const { onlyUsers } = require("../app/middlewares/session");
+const {
+  allowingEditingOfProducts,
+  checkBeforeModifyProduct,
+} = require("../app/validators/session");
+
 // Search
 routes.get("/search", searchController.index);
 
 //Products
 routes.get("/create", onlyUsers, productsController.create);
 routes.get("/:id", productsController.show);
-routes.get("/:id/edit", productsController.edit);
+routes.get("/:id/edit", allowingEditingOfProducts, productsController.edit);
 
 routes.post("/", multer.array("photos", 6), productsController.post);
-routes.put("/", multer.array("photos", 6), productsController.put);
-routes.delete("/", productsController.delete);
+routes.put(
+  "/",
+  multer.array("photos", 6),
+  checkBeforeModifyProduct,
+  productsController.put
+);
+routes.delete("/", checkBeforeModifyProduct, productsController.delete);
 
 module.exports = routes;
